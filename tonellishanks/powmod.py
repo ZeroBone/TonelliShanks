c2eb1a25a30e1f66b7e29147ea41bd0bddc2241b
ZeroBone <zerobone21@gmail.com>
2022-09-15 17:28:49 +0200

    Minor improvement to the fast exponentiation algorithm

--- tonellishanks/powmod.py
+++ tonellishanks/powmod.py
@@ -4,15 +4,11 @@ def power_modulo(a: int, b: int, n: int) -> int:
 
     # Loop through all the binary digits of the numbers
     while b != 0:
-
         if b % 2 == 1:
             # b odd
             result = (result * a) % n
-            # result = (result * (a % n)) % n
 
         a = (a * a) % n
-        # a = ((a % n) * (a % n)) % n
-
-        b //= 2
+        b >>= 1
 
     return result
